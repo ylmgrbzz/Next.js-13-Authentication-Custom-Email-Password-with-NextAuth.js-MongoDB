6f85411cc8856da0d8d7773828478c5acd7dd27d
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2024-09-06 18:17:47 +0300

    chore: Add authentication route for NextAuth

---
+++ app/api/auth/[...nextAuth]/route.js
@@ -0,0 +1,54 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+          if (!user) {
+            throw new Error("No user found");
+          }
+
+          const isValid = await bcrypt.compare(password, user.password);
+          if (!isValid) {
+            throw new Error("Invalid password");
+          }
+
+          return { email: user.email, name: user.name };
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+    }),
+  ],
+  secret: process.env.SECRET,
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    secret: process.env.SECRET,
+  },
+  callbacks: {
+    async jwt(token, user) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session(session, token) {
+      session.id = token.id;
+      return session;
+    },
+  },
+};
